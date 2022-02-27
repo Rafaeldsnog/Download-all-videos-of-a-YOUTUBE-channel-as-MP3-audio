f03c245115650ff7420af070d83436ad8f583942
Rafa <65832023+Rafaeldsnog@users.noreply.github.com>
2022-02-27 13:37:14 -0300

    Add files via upload

---
+++ yt_channel_audio.py
@@ -0,0 +1,49 @@
+from pytube import YouTube
+from pytube import Playlist
+import math
+import os
+
+# INSERT THE PLAYLIST LINK OF THE CHANNEL
+channel = input("URL of the channel you want to download all the audios(Playlist Link): ")
+count = 0
+
+def AudioDownload(video_link, destination, amount):
+    audios = YouTube(video_link)
+
+    audio = audios.streams.filter(only_audio=True).first()
+
+    out_file = audio.download(output_path=destination)
+
+    # conversion to mp3 file
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+
+    global count
+    count = count + 1
+
+    #function to display progress
+    progress(count, amount)
+
+def DownloadChannel(channel_link):
+    list_videos = Playlist(channel_link)
+    amount = len(list_videos)
+    # choose the directory you want to download the files
+    print("Enter the destination (leave blank for current directory)")
+    destination = str(input(">> ")) or "."
+
+    for url in list_videos:
+        AudioDownload(url, destination, amount)
+
+def progress(count, amount):
+    # width of the progress bar
+    width = 40;
+    percent=count/amount
+    left = math.ceil(width * (percent))
+    right = width - left
+    tags = '#' * left
+    spaces = " " * right
+    percents = f"{(percent*100):.0f}%"
+    print(" Downloaded: ", count, "/", amount, ". Progress: [",tags,spaces,"]", percents,"\r", end="")
+
+DownloadChannel(channel)
